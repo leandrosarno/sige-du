92c6fdec958a57d4962439206dc17df9299dd81c
SIG-EDU <leandrosarno.7741901@edu.sme.prefeitura.sp.gov.br>
2026-08-26 14:01:34 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Arquivo Global de Configurações do Sistema - CEU
+const API_URL = "https://script.google.com/macros/s/AKfycbyUqh6yg6wKCHPL4LzIA3kS5y7d2Md0Ll9P9zG5tue3km-25q636qAwDodXpVE2nCSf/exec";
